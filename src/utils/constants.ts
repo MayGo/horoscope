f26455d10a29459ddfad024b5d46b0c3b82aee8e
Maigo Erit <maigo.erit@gmail.com>
2025-02-21 12:29:31 +0200

    Update Privacy Policy and Terms & Conditions links

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,6 +1,6 @@
 export const PRIVACY_POLICY_LINK =
-    'https://spice-echinacea-ba4.notion.site/Privacy-Policy-181cdd302b4a8001959ff457394587f6';
+    'https://spice-echinacea-ba4.notion.site/Privacy-Policy-1a1cdd302b4a80b2baeadcc1b5c2cae6?pvs=4';
 export const TERMS_AND_CONDITIONS_LINK =
-    'https://spice-echinacea-ba4.notion.site/Terms-Conditions-181cdd302b4a805994b6e8499b76ceca?pvs=4';
+    'https://spice-echinacea-ba4.notion.site/Terms-Conditions-1a1cdd302b4a80dd9a86f612d544fcb6?pvs=4';
 
 export const EMAIL_TO = 'info@trimatech.ee';
